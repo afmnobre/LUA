80c9b4541c64b97ac0283136c011b95db36bc93e
afmnobre <lucas_guitar1987@hotmail.com>
2023-10-15 16:33:04 -0300

    Aula 3

---
+++ CFBCursos/aula_03.lua
@@ -0,0 +1,12 @@
+print("Digite seu nome: ")
+nome=io.read()
+
+print("Digite um valor:")
+vl1=io.read("*number")
+print("Digite outro valor:")
+vl2=io.read("*number")
+
+print(nome,"a soma de", vl1, "com",vl2, "é igual a", vl1 + vl2)
+
+print(nome)
+print(type(nome))
