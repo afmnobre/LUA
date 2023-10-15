8a6fa69f9acadfbe6dc7f88c89d0fd31216705ef
afmnobre <lucas_guitar1987@hotmail.com>
2023-10-15 01:26:55 -0300

    Curso Basico de Lua

---
+++ CFBCursos/aula_02.lua
@@ -0,0 +1,20 @@
+print("VARIAVEIS EM LUA")
+
+nome="Lucas"
+num=10
+valor=5.2
+logica=true
+vazia=nil
+
+num1=10
+num2=5
+soma=num1+num2
+print("Soma de ",num1," com ",num2," e igual a ",soma)
+
+print("Tipo:",type(nome),"Valor:",nome)
+print("Tipo:",type(num),"Valor:",num)
+print("Tipo:",type(logica),"Valor:",logica)
+print("Tipo:",type(vazia),"Valor:",vazia)
+print("Tipo:",type(valor),"Valor:",valor)
+print("Tipo:",type(type))
+
