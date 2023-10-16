3de5079a23e56a0e483eac9e4148326e1b3b1aaf
afmnobre <lucas_guitar1987@hotmail.com>
2023-10-16 00:22:20 -0300

    Operadores em Lua

---
+++ CFBCursos/aula_06.lua
@@ -0,0 +1,88 @@
+print("++++++++++++++++++++++++++++++++++++++")
+print("\n   Aprendendo Operadores Matematicos:\n")
+print("++++++++++++++++++++++++++++++++++++++")
+print(" \
+    Soma:           + \n \
+    Subtração:      - \n \
+    Divisão:        / \n \
+    Multiplicação:  * \n \
+    Exponenciação:  ^ \n \
+    Resto:          % \n \
+    ")
+
+
+print("++++++++++++++++++++++++++++++++++++++")
+print("O numero na variavel valor1 é 10 e \nna variavel valor 2 é 2")
+
+valor1=10
+valor2=2
+
+print("\nSOMA++++++++++++++++++++++++++++++++++")
+soma=(valor1+valor2)
+print(soma)
+
+print("++++++++++++++++++++++++++++++++++++++")
+print("SUBTRAÇÃO+++++++++++++++++++++++++++++")
+subtracao=(valor1-valor2)
+print(subtracao)
+
+print("++++++++++++++++++++++++++++++++++++++")
+print("DIVISÃO+++++++++++++++++++++++++++++++")
+divisao=(valor1/valor2)
+print(divisao)
+
+print("++++++++++++++++++++++++++++++++++++++")
+print("MULTIPLICAÇÃO+++++++++++++++++++++++++")
+multiplicacao=(valor1*valor2)
+print(multiplicacao)
+
+print("++++++++++++++++++++++++++++++++++++++")
+print("EXPONENCIAÇÃO+++++++++++++++++++++++++")
+exponenciacao=(valor1^valor2)
+print(exponenciacao)
+
+print("++++++++++++++++++++++++++++++++++++++")
+print("RESTO+++++++++++++++++++++++++++++++++")
+resto=(valor1%valor2)
+print(resto)
+
+
+print("\n\n++++++++++++++++++++++++++++++++++++++")
+print("\n   Aprendendo Operadores Relacionais:\n")
+print("++++++++++++++++++++++++++++++++++++++")
+print(" \
+    Maior:           > \n \
+    Menor:           < \n \
+    Menor ou Igual:  <= \n \
+    Maior ou Igual:  >= \n \
+    Igual:           == \n \
+    Diferente:       ~= \n \
+    ")
+
+
+
+
+print("\n\n++++++++++++++++++++++++++++++++++++++")
+print("\n   Aprendendo Operadores Lógicos:\n")
+print("++++++++++++++++++++++++++++++++++++++")
+print(" \
+    AND:           > \n \
+    OR:            < \n \
+    NOT:           <= \n \
+    NIL:           >= \n \
+    ")
+
+print("\n\n++++++++++++++++++++++++++++++++++++++")
+print("\n     ORDEM DE PRECEDENCIA:          \n")
+print("++++++++++++++++++++++++++++++++++++++")
+print(" \
+    1. ()                \n \
+    2. ^                 \n \
+    3. not #             \n \
+    4. * / %             \n \
+    5. * -               \n \
+    6. ..                \n \
+    7. < > >= <= == ~=   \n \
+    8. and               \n \
+    9. or                \n \
+    ")
